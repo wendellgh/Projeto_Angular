74aa9db44101ebeb1fbe1f7c29c1de2751bb3c25
Wendell Mendes <wendellgh@gmail.com>
2023-01-10 10:50:14 -0300

    Components criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { HeaderComponent } from './modules/home/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
