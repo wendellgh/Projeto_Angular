f0da87bc6cdfea3baf282f0b2421d9fb3ce98f98
Wendell Mendes <wendellgh@gmail.com>
2023-01-10 16:25:32 -0300

    Scss pronto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
+
 import { HeaderComponent } from './modules/home/components/header/header.component';
+import { HomeModule } from './modules/home/home.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//Components
 import { HeaderComponent } from './components/header/header.component';
 import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
 import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+//Page
+import { HomeComponent } from './pages/home/home.component';
 
 
 
@@ -12,7 +16,8 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
     HeaderComponent,
     TodoButtonDeleteAllComponent,
     TodoInputAddItensComponent,
-    TodoListComponent
+    TodoListComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
