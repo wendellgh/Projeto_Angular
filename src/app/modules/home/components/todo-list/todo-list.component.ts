c9b5c5d70ed02efc1319ca3c2a7dbd866ac0f7a8
Wendell Mendes <wendellgh@gmail.com>
2023-01-11 13:10:44 -0300

    Iniciando as Task

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//Interface
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+
+  public taskList:Array<TaskList> = [
+    {task:"Nova Task", checked:true},
+    {task:"Nova Task 2", checked:false}
+  ];
+
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
 
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 //Components
 import { HeaderComponent } from './components/header/header.component';
@@ -8,6 +9,7 @@ import { TodoInputAddItensComponent } from './components/todo-input-add-itens/to
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 //Page
 import { HomeComponent } from './pages/home/home.component';
+
 
 
 
@@ -20,7 +22,8 @@ import { HomeComponent } from './pages/home/home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
